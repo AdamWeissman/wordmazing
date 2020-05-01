350884bda15d928ec9045b78f381aaa76e46a981
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-04-30 22:12:03 -0400

    user model and word model but no migrations run yet

---
+++ db/migrate/20200501020745_create_words.rb
@@ -0,0 +1,12 @@
+class CreateWords < ActiveRecord::Migration[6.0]
+  def change
+    create_table :words do |t|
+      t.string :the_word
+      t.string :word_activation_switch
+      t.integer :the_word_score
+      t.boolean :cycle_now, default: false
+
+      t.timestamps
+    end
+  end
+end
