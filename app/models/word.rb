b138082c44f8459592730c5b7b6dd079e95855e4
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-05-01 00:39:43 -0400

    the join table... well, really a model seems to be working... actually all models seem to be working according to plan

--- app/models/word.rb
+++ app/models/word.rb
@@ -3,10 +3,10 @@ class Word < ApplicationRecord
   has_many :wordletters
   has_many :letters, through: :wordletters
 
-  def self.make_letters
+  def make_letters
     self.the_word.split(//).each do |letter|
-      l = Letter.find_or_create_by(:the_letter => letter)
-      Wordletter.find_or_create_by(:user_id => user.id, :word_id => word.id, :letter_id => l.id)
+      l = Letter.find_or_create_by(:the_letter => letter, :user_id => self.user_id)
+      Wordletter.find_or_create_by(:user_id => user.id, :word_id => self.id, :letter_id => l.id)
     end
   end
-endex
+end
