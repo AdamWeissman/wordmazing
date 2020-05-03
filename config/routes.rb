afc6c1219874b77d512517783f6e3d7707b7c30f
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-05-03 14:31:12 -0400

    resources is now minimally correct

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:create, :destroy] do
-        resources :words, only: [:index, :create, :update]
-        resources :letters, only: [:index, :create, :update]
-        resources :wordletters, only: [:index, :create, :update]
+      resources :users, only: [:create, :update:, :destroy] do
+        resources :words, only: [:create, :update]
+        resources :letters, only: [:create, :update]
+        resources :wordletters, only: [:create, :update]
 
         resources :readymades, only: [:index, :create, :update, :destroy] #this is only for admin 
         resources :spokenmessages, only: [:index, :create, :update, :destroy] #this is only for admin
