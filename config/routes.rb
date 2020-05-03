a788cc0e4d67d84880be73aeb5c93ab08f9f7f2f
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-05-03 11:16:19 -0400

    setting up routes, prior to json

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only [:create, :destroy] do
+        resources :words, only [:index, :create, :update]
+        resources :letters, only [:index, :create, :update]
+        resources :wordletters, only [:index, :create, :update]
+      end
+      resources :readymades, only [:index]
+      resources :spokenmessages, only [:index]
+    end
+  end
+
+
 end
