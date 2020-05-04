856af43b5eb1a14da9129a7acf4812ab93ac1b73
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-05-04 14:43:59 -0400

    render json code included

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -2,6 +2,11 @@ class UserController < Api::V1::MasterController
   
   def create
     user = User.new(user_params)
+    if user.save
+      render json: user, status: accepted
+    else
+      render json: {errors: user.errors.full_messages}, status: :unprocessible_entity
+    end
   end
 
   def destroy
