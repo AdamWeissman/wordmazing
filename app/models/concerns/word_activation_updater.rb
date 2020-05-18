ae580d8b5a04996228cb6e290be440f2c9a66c28
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-05-18 14:12:43 -0400

    got activation switch matcher thing working on repl... now time to integrate

--- app/models/concerns/word_activation_updater.rb
+++ app/models/concerns/word_activation_updater.rb
@@ -1,10 +1,38 @@
 module WordActivationUpdater
+  
   def update_word_activation_scores(those_word_ids, that_letter_to_change, that_letter_score, the_user)
     those_word_ids.each do |x|
       word_to_change = the_user.words.find_by_id(x)
       reference_word = word_to_change.the_word
-      #get the indexes for each letter that matches that_letter_to_change ... test on repl
       activation_switch = word_to_change.word_activation_switch
+      
     end
   end
+
+  def activation_update(word, activator, letter, letter_score)
+    letter_array = word.split("")
+    activator_array = activator.split("")
+  
+    activation_update_locations = []
+    letter_array.each_with_index do |l, index|
+      if l == letter
+        activation_update_locations << index
+      else
+        next
+      end
+    end
+  
+    activation_key_update = []
+    activator_array.each_with_index do |l, index|
+      if activation_update_locations.include?(index) 
+        activation_key_update << letter_score
+      else
+        activation_key_update << l
+      end    
+    end
+  
+    return [word, activator, letter, letter_score, activation_key_update.join.to_s]
+  
+  end
+
 end
