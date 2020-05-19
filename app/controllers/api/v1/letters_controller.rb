485681e65bc9a2ed845bd64dd060c9570cdb4761
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-05-19 09:22:42 -0400

    may need to have error logic on the JS so if the data it's looking for isn't passed, then it will summon words controller

--- app/controllers/api/v1/letters_controller.rb
+++ app/controllers/api/v1/letters_controller.rb
@@ -5,36 +5,24 @@ class Api::V1::LettersController < ApplicationController
     @user = User.find(params[:user_id])
     @letters = @user.letters.all
 
+    # if @letters.size < 2
+    #   alphabet = ('A'..'Z').to_a.shuffle
+    #   a_random_letter = alphabet[0] != these_ones.size[0] ? alphabet[0] : alphabet[1]
+    #   random_letter_object = @user.letters.find_or_create(user_id: @user.id, the_letter: a_random_letter.to_s, the_letter_score: 9)
+    #   random_letter_object.save
+    #   redirect_to '/index'
+    # else  
 
-    def under_a_certain_score(the_letters)
-      these_ones = the_letters.map {|i| i if i.the_letter_score < 3}
-      if these_ones.size >= 2
+      def under_a_certain_score(the_letters)
+        these_ones = the_letters.map {|i| i if i.the_letter_score < 3}
         return these_ones.shuffle[0...2]
-      elsif these_ones.size == 1
-        alphabet = ('A'..'Z').to_a.shuffle
-        a_random_letter = alphabet[0] != these_ones.size[0] ? alphabet[0] : alphabet[1]
-        random_letter_object = @user.letters.find_or_create_by(user_id: @user.id, the_letter: a_random_letter, the_letter_score: 20)
-        these_ones << random_letter_object
-        return these_ones.shuffle[0...2]
-      else
-        alphabet = ('A'..'Z').to_a.shuffle
-        a_random_letter = alphabet[0] != these_ones.size[0] ? alphabet[0] : alphabet[1]
-        random_letter_object = @user.letters.find_or_create_by(user_id: @user.id, the_letter: a_random_letter, the_letter_score: 20)
-        these_ones << random_letter_object
-        return these_ones.shuffle[0...2]
-      end
-    end  
-
+      end  
 
     #NEED TO ALSO CHECK IF CYCLE_NOW is true for any words... if so, that word should enter rotation before all letters are finished, and be paired with a random word
     #(need a redirect to words index which will mimic under a certain score) 
     @letters = under_a_certain_score(@letters)
-    
-    if @letters != nil
-      render json: @letters 
-    else
-      redirect_to "http://www.rubyonrails.org"
-    end
+    render json: @letters 
+
   end
 
   def update
