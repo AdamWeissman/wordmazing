9d2e77bd36fef787d4991d648033a5439103f2ee
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-05-23 10:40:52 -0400

    repl solution for regex

--- app/models/concerns/word_activation_updater.rb
+++ app/models/concerns/word_activation_updater.rb
@@ -35,6 +35,14 @@ module WordActivationUpdater
     activation_key_update = activation_key_update.join.to_s
     word_object.word_activation_switch = activation_key_update
     word_object.save
+    if word_object.word_activation_switch.match?(/0{#{word_object.word_activation_switch.length.to_i}}/)
+      word_object.cycle_now = true
+      word_object.save
+      #will this work
+    else
+      word_object.cycle_now = false
+      word_object.save
+    end
   end
 
 end
