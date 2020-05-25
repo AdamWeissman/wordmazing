7647729317b07a52b985f5dd4168495c05fc4907
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-05-25 14:57:08 -0400

    another try and catch error

--- my-front-end/app.js
+++ my-front-end/app.js
@@ -183,15 +183,18 @@ async function random_two_letter_func_v2 () {
   random_two_words = []
   let x = activeUserID[0];
   //alert("Return Random Two Letters With Low Scores");
-  const result = await fetch(`${USERS_URL}/${x}/letters`)
-  const data = await result.json()
+ 
   
   try {
+  const result = await fetch(`${USERS_URL}/${x}/letters`)
+  const data = await result.json()
   const the_letters = await data.letters
   const two_random_letters = await the_letters.forEach(element => {
       random_two_letters.push(element.the_letter);
       });}
   catch {
+    const result = await fetch(`${USERS_URL}/${x}/words`)
+    const data = await result.json()
     const the_words = await data.words
     const two_random_words = await the_words.forEach(element => {
         random_two_words.push(element.the_word);
