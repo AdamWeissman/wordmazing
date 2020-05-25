3a1134c9e0efcc220e3b54ba3842df309bc0c086
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-05-25 12:40:39 -0400

    corrected misuse of parens

--- my-front-end/app.js
+++ my-front-end/app.js
@@ -214,47 +214,46 @@ async function check_cycle_now_func () {
   words_and_or_letters = new Set()
   let x = activeUserID[0];
   const result = await fetch(`${USERS_URL}/${x}/letters`)
+  const data = await result.json()
   // const data = await result.json()
   // const the_letters = await data.letters
   // const the_words = await data.words
   //const what_is_cycling = 
     try {
-      const data = await result.json()
       const the_letters = await data.letters
       const the_words = await data.words
-      (the_words.forEach(element => {
+      {the_words.forEach(element => {
         if (element.cycle_now === true) {words_and_or_letters.add("words")}
-      })),
+      }),
 
-      (the_letters.forEach(element => {
+      the_letters.forEach(element => {
         if (element.cycle_now === true) {words_and_or_letters.add("letters")}
-      }))
+      })};
       
       x = Array.from(words_and_or_letters)
       return x
     }
 
-    catch {      
-      const data = await result.json()
-      
-      
+    catch {            
       try {
         const the_words = await data.words
-        (the_words.forEach(element => {
-      if (element.cycle_now === true) {words_and_or_letters.add("words")}
-      }))
+        the_words.forEach(element => {
+        if (element.cycle_now === true) {words_and_or_letters.add("words")}
+        });
       
       x = Array.from(words_and_or_letters)
-      return x}
+      return x
+      }
 
       catch {
         const the_letters = await data.letters
-        (the_letters.forEach(element => {
+        the_letters.forEach(element => {
         if (element.cycle_now === true) {words_and_or_letters.add("letters")}
-        }))
+        });
         
         x = Array.from(words_and_or_letters)
-        return x}
+        return x
+      }
     }
 
 }
