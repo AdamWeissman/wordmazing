eabfdb0d7978f9a56729802060d16e4d377b77b6
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-05-27 14:52:41 -0400

    speech js was creating new issues will deal with OO once I can get it all to work

--- my-front-end/app.js
+++ my-front-end/app.js
@@ -1,3 +1,39 @@
+var synth = window.speechSynthesis;
+var textToSpeak = []
+
+function speak() {  
+  if (synth.speaking) {
+        console.error('speechSynthesis.speaking');
+        return;
+    }
+    if (textToSpeak[0] !== '') {
+    var voices = synth.getVoices();
+    var utterThis = new SpeechSynthesisUtterance(textToSpeak[0]) 
+    try {
+      utterThis.pitch = 1.0;
+      utterThis.rate = .9;
+      utterThis.voice = voices[49]}
+    catch {
+      utterThis.pitch = 1.7;
+      utterThis.rate = .7;
+      utterThis.voice = voices[0]}
+      //leaves voice as default
+    }
+    utterThis.onend = function (event) {
+        console.log('SpeechSynthesisUtterance.onend');
+    } 
+    utterThis.onerror = function (event) {
+        console.error('SpeechSynthesisUtterance.onerror');
+        console.log(utterThis)
+    }
+
+    synth.speak(utterThis);
+    console.log(utterThis)
+  }
+
+
+
+
 
 
 
